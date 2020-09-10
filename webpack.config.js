be8ff89ba5da49ebe254d796859d89c0ebb18358
woai3c <411020382@qq.com>
2020-09-10 18:10:48 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: path.resolve('./src/loader.js'),
+                    }
+                ]
+            }
+        ]
+    },
+}
